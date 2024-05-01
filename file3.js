7922b350d34396b9cb2c203be28f16b0235eecb0
MOHAMMAD ZEESHAN <zeeshanansari347@gmail.com>
2024-05-01 14:36:05 +0530

    JSON

---
+++ file3.js
@@ -0,0 +1,11 @@
+let obj = {
+"name" : "whitehat", 
+"email" : "whitehat@gmail.com",
+"age" : 24,
+"city" : "swm"
+}
+
+console.log(obj.name)
+console.log(obj.email)
+console.log(obj.age)
+console.log(obj.city)
